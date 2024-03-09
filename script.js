589338d6362c14be56f3fbd95324bee5b548d96f
Guilherme Silva Madeira <161243338+guilhermemadeira1@users.noreply.github.com>
2024-03-08 21:46:28 -0300

    alterações no código
    alterações no código projeto-lanchonete

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ var busc = txtbusca.value
 var busca = busc.toUpperCase()
 //colocar as variáveis dentro das função para não haver erro.
 var sel = document.querySelector('select#opçoes')
-sel.addEventListener('click', valores)
+//sel = opções de preços
+sel.addEventListener('click', selecionarPreços)
 
-function valores(){
-    
+sel.value=""
+//quando a página carregar o select recebe opção vazia
+
+function selecionarPreços(){
+
+    var msg = document.querySelector("div#mensagem")
     var caixaimg = document.querySelector('div#imagem')
+    var opção = document.getElementsByName('lista')
+
     var sel = document.querySelector('select#opçoes')
     var H = document.getElementById('hamburguer')
     var C = document.getElementById('cachorro-quente')
@@ -22,13 +29,67 @@ function valores(){
         P.style.display = 'none'
         B.style.display = 'none'
 
+        H.src = 'imagens/hamburguer.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         H.style.transition = '0.3s'
         /*transição na primeira vez que clicar*/
         caixaimg.style.width= '300px'
         /*diminuir tamanho da caixa de imagens*/
+        opção[0].checked = true
+        //marca caixa de opção Hambúrguer
+        msg.innerHTML = sel[0].innerHTML
+        //mostra uma mensagem com o mesmo texto da opção Hambúrguer
     }
+    else if (sel.value =='cachorro-quente'){
+        C.style.display = 'none'
+        P.style.display = 'none'
+        B.style.display = 'none'
+
+        H.src = 'imagens/cachorroquente.jpg'
+        H.style.height = '200px'
+        H.style.width = '200px'
+        H.style.transition = '0.3s'
+        
+        caixaimg.style.width= '300px'
+        opção[1].checked = true
+        //marca opção Cachorro-quente
+        msg.innerHTML = sel[1].innerHTML
+         //mostra uma mensagem com o mesmo texto da opção Cachoro-quente
+
+    }
+    else if (sel.value == 'pizza'){
+        C.style.display = 'none'
+        P.style.display = 'none'
+        B.style.display = 'none'
+
+        H.src = 'imagens/pizza.jpg'
+        H.style.height = '200px'
+        H.style.width = '200px'
+        H.style.transition = '0.3s'
+
+        caixaimg.style.width = '300px'
+        opção[2].checked = true
+        //marca opção Pizza
+        msg.innerHTML = sel[2].innerHTML
+         //mostra uma mensagem com o mesmo texto da opção Pizza
+    }
+    else if (sel.value == "batata-frita"){
+        C.style.display = 'none'
+        P.style.display = 'none'
+        B.style.display = 'none'
+
+        H.src = 'imagens/batata.jpg'
+        H.style.width = '200px'
+        H.style.height = '200px'
+        H.style.transition = '0.3s'
+
+        caixaimg.style.width = '300px'
+        opção[3].checked = true
+
+        msg.innerHTML = sel[3].innerHTML
+    }
+
 }
 
 function selecionar(){
@@ -46,7 +107,7 @@ function selecionar(){
     if(opção[0].checked){
         msg.innerHTML='Você escolheu: Hambúrguer.'
         
-        H.src = 'hamburguer.jpg'
+        H.src = 'imagens/hamburguer.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         H.style.transition = '0.3s'
@@ -62,7 +123,7 @@ function selecionar(){
     }
     else if(opção[1].checked){
         msg.innerHTML='Você escolheu: Cachorro-quente.'
-        H.src = 'cachorroquente.jpg'
+        H.src = 'imagens/cachorroquente.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         H.style.transition = '0.3s'
@@ -76,7 +137,7 @@ function selecionar(){
     }
     else if(opção[2].checked){
         msg.innerHTML='Você escolheu: Pizza.'
-        H.src = 'pizza.jpg'
+        H.src = 'imagens/pizza.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         H.style.transition = '0.3s'
@@ -90,7 +151,7 @@ function selecionar(){
     }
     else if(opção[3].checked){
         msg.innerHTML='Você escolheu: Batata-Frita.'
-        H.src = 'batata.jpg'
+        H.src = 'imagens/batata.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         H.style.transition = '0.3s'
@@ -123,7 +184,7 @@ var busca = busc.toUpperCase()
       case "HAMBURGUER":
        case "HAMBÚRGUER":
         msg.innerHTML= "Boa escolha! Compre nosso hambúrger."
-        H.src = 'hamburguer.jpg'
+        H.src = 'imagens/hamburguer.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
         
@@ -143,7 +204,7 @@ var busca = busc.toUpperCase()
          case "HOT DOG":
           case "HOTDOG":
         msg.innerHTML="Boa escolha! Compre nosso Cachorro-quente." 
-        H.src = 'cachorroquente.jpg'
+        H.src = 'imagens/cachorroquente.jpg'
         H.style.height = '200px'
         H.style.width = '200px'
 
@@ -159,7 +220,7 @@ var busca = busc.toUpperCase()
 
       case "PIZZA":
          msg.innerHTML="Boa escolha! Compre nossa Pizza crocante."
-         H.src = 'pizza.jpg'
+         H.src = 'imagens/pizza.jpg'
          H.style.height = '200px'
          H.style.width = '200px'
     
@@ -177,7 +238,7 @@ var busca = busc.toUpperCase()
        case "BATATA-FRITA":
          case "BATATA":
          msg.innerHTML="Boa escolha! Compre nossa porção de batata-frita."
-         H.src = 'batata.jpg'
+         H.src = 'imagens/batata.jpg'
          H.style.height = '200px'
          H.style.width = '200px'
  
@@ -202,11 +263,38 @@ var busca = busc.toUpperCase()
 }
 function limpar(){
     var opção = document.getElementsByName('lista')
+    var caixaimg = document.querySelector('div#imagem')
+    var sel = document.querySelector('select#opçoes')
+    var H = document.getElementById('hamburguer')
+    var C = document.getElementById('cachorro-quente')
+    var P = document.getElementById('pizza')
+    var B = document.getElementById('batata')
+    var sel = document.querySelector('select#opçoes')
+    var msg =document.querySelector('div#mensagem')
+
     document.querySelector('input#busca').value= ""
     //o input de texto recebe string vazia.
-    location.reload()
-    // recarrega a página instantaneamente
-    var msg =document.querySelector('div#mensagem')
+    sel.value = ""
+    //preço fica vazio
+    caixaimg.style.width = '100%'
+
+    for(i in opção){
+        //cada opção funciona como uma lista
+        opção[i].checked = false
+    }
+    //desmarcar opções quando quando limpar
+
+    H.style.display = 'inline-block'
+    C.style.display = 'inline-block'
+    P.style.display = 'inline-block'
+    B.style.display = 'inline-block'
+
+    H.style.width = '100px'
+    H.style.height = '100px'
+    H.style.transition = 'none'//retirar animação
+
+    //a imagem dos pedidos selecionados são substituidos pela imagem do hamburguer, por isso só precisa mudar o tamanho desta enquanto as outras ficam visíveis como bloco em linha (inline-block)
+
     msg.innerHTML='Escolha novamente'
 }
 
@@ -246,7 +334,7 @@ function pedir(){
             case "HAMBÚRGUER":
             case "HAMBURGUER":
                 msg.innerHTML='Pedido concluído! Aproveite seu Hambúrguer.'
-                H.src = 'hamburguer.jpg'
+                H.src = 'imagens/hamburguer.jpg'
                 H.style.height = '200px'
                 H.style.width = '200px'
                 
@@ -265,7 +353,7 @@ function pedir(){
             case "HOT-DOG":
             case "HOTDOG":
                 msg.innerHTML='Pedido concluído. Aproveite seu Cachorro-quente.'
-                H.src = 'cachorroquente.jpg'
+                H.src = 'imagens/cachorroquente.jpg'
                 H.style.height = '200px'
                 H.style.width = '200px'
                 
@@ -280,7 +368,7 @@ function pedir(){
                  break
             case "PIZZA":
                 msg.innerHTML='Pedido concluído. Aproveite sua Pizza.'
-                H.src = 'pizza.jpg'
+                H.src = 'imagens/pizza.jpg'
                 H.style.height = '200px'
                 H.style.width = '200px'
                 
@@ -297,7 +385,7 @@ function pedir(){
              case "BATATA FRITA":
              case "BATATA":
                  msg.innerHTML= 'Pedido concluído. Aproveite sua Batata-frita.'
-                 H.src = 'batata.jpg'
+                 H.src = 'imagens/batata.jpg'
                  H.style.height = '200px'
                  H.style.width = '200px'
                  
